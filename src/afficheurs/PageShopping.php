c691dcda5979ca574a582592228c1632fd1600aa
miesch3u <nathanaelmiesch@gmail.com>
2023-02-16 14:32:47 +0100

    réparation du code a la bob the builder

--- src/afficheurs/PageShopping.php
+++ src/afficheurs/PageShopping.php
@@ -14,10 +14,19 @@ class PageShopping extends Afficheur
     public function execute(): string
     {
         $html = "<div style='display: flex; flex-direction: column'><div style='display: flex; flex-direction: row'><button id='rech'type='button'>Rechercher</button>"
+            ."<input id='rechTxt'>"
             ."<select id='filter'><option value='0'>Filtrer</option><option value='1'>Confiseries</option><option value='2'>Boissons</option>"
             ."<option value='3'>Hygiène</option><option value ='4'>Cosmetique</option><option value='5'>Fromages et pâtés</option></select></div>"
             ."<section style='display: flex;justify-content: space-around; flex-direction: column; flex-wrap: wrap'>";
-        $req = $this->db->prepare("SELECT id,nom,prix,lieu from produit");
+        $cond = "";
+        if (isset($_GET['cat'])){
+            $cat = $_GET['cat'];
+            $key = $_GET['key'];
+            $cond = 'where ';
+            if($cat>0) $cond.= " categorie = $cat and ";
+            $cond.= "nom like '%$key%'";
+        }
+        $req = $this->db->prepare("SELECT id,nom,prix,lieu from produit ".$cond);
         $req->execute();
         while($row = $req->fetch()){
             //Création des articles

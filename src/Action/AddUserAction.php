a211275e95fb75de71d5ff4fe00e7935f3468b7e
Vivien <vivien.stemmelen2@etu.univ-lorraine.fr>
2023-02-16 12:09:28 +0100

    suppression use inutiles

--- src/Action/AddUserAction.php
+++ src/Action/AddUserAction.php
@@ -8,8 +8,6 @@ use mywishlist\Action\Action;
 use mywishlist\db\ConnectionFactory;
 use mywishlist\Auth\Authentication;
 
-use db\ConnectionFactory;
-use Auth\Authentication;
 class AddUserAction extends Action
 {
 

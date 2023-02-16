e5a43771e8f14b664a796bf4a28bd91bf025a01b
Egario-akier <thierry.dissidia@gmail.com>
2023-02-16 13:12:01 +0100

    Premier commit de PageArticle

--- src/afficheurs/PageArticle.php
+++ src/afficheurs/PageArticle.php
@@ -1,11 +1,31 @@
 <?php
 
 namespace mywishlist\afficheurs;
+use mywishlist\db\ConnectionFactory;
 
 class PageArticle extends Afficheur
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->db = ConnectionFactory::makeConnection();
+    }
+
     public function execute(): string
     {
-        return "ici c'est l'article'";
+        $html ="";
+        $action = $_GET['id'];
+        $req = $this->db->prepare("SELECT * from produit where id=?");
+        $req->bindParam(1,$action);
+        $req->execute();
+        $row = $req->fetch();
+        //Création des articles
+        $id = $row['id'];
+        $name = $row['nom'];
+        $prix = $row['prix'];
+        $lieu =$row['lieu'];
+        $html.="<div><strong>$name</strong><br>Prix : $prix<br>Provenance : $lieu<img class='image_pdt' src=\"src/img/$id.jpg\" alt=\"Image\"></div>";
+
+        return $html;
     }
 }
--- src/afficheurs/PageShopping.php
+++ src/afficheurs/PageShopping.php
@@ -13,9 +13,9 @@ class PageShopping extends Afficheur
 
     public function execute(): string
     {
-        $html = "<div style='display: flex; flex-direction: column'><div style='display: flex; flex-direction: row'><button id='rech'type='button'>rechercher</button>"
-            ."<select id='filter'><option value='0'>filtrer</option><option value='1'>confiseries</option><option value='2'>boissons</option>"
-            ."<option value='3'>higiene</option><option value='5'>fromages et pâté</option></select></div>"
+        $html = "<div style='display: flex; flex-direction: column'><div style='display: flex; flex-direction: row'><button id='rech'type='button'>Rechercher</button>"
+            ."<select id='filter'><option value='0'>Filtrer</option><option value='1'>Confiseries</option><option value='2'>Boissons</option>"
+            ."<option value='3'>Hygiène</option><option value='5'>Fromages et pâtés</option></select></div>"
             ."<section style='display: flex;justify-content: space-around; flex-direction: column; flex-wrap: wrap'>";
         $req = $this->db->prepare("SELECT id,nom,prix,lieu from produit");
         $req->execute();
@@ -25,8 +25,7 @@ class PageShopping extends Afficheur
             $name = $row['nom'];
             $prix = $row['prix'];
             $lieu =$row['lieu'];
-            $html.="<div><strong>$name</strong><br>Prix : $prix<br>Provenance : $lieu<img class='image_pdt' src=\"src/img/$id.jpg\" alt=\"Image\"></div>";
-
+            $html.="<div><strong>$name</strong><br>Prix : $prix<br>Provenance : $lieu<a href='index.php?action=article&id=$id'><img class='image_pdt' src=\"src/img/$id.jpg\" alt=\"Image\"></div></a>";
         }
 
 

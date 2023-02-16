4e39a11526e51abeaad8d4c014849a0408f0068f
miesch3u <nathanaelmiesch@gmail.com>
2023-02-16 22:08:52 +0100

    suppression d'un article

--- src/afficheurs/PageArticle.php
+++ src/afficheurs/PageArticle.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace mywishlist\afficheurs;
+
 use mywishlist\db\ConnectionFactory;
 
 class PageArticle extends Afficheur
@@ -13,24 +14,25 @@ class PageArticle extends Afficheur
 
     public function execute(): string
     {
-        $html ="";
+        $html = "";
         $action = $_GET['id'];
         $req = $this->db->prepare("SELECT * from produit where id=?");
-        $req->bindParam(1,$action);
+        $req->bindParam(1, $action);
         $req->execute();
         $row = $req->fetch();
         //Création des articles
         $id = $row['id'];
         $name = $row['nom'];
         $prix = $row['prix'];
-        $lieu =$row['lieu'];
+        $lieu = $row['lieu'];
         $desc = $row['description'];
         $det = $row['detail'];
         $discircuit = $row['distance'];
-        $html.= "<style>.mineur {margin-left: 20px;}</style><h1 class=\"prod\">$name</h1><div class=\"mineur\"><p><br>Prix : $prix €<br>Provenance : $lieu<br>Description : <br><br>$desc<br>".
-            "<br>Détails : $det<br>Distance du lieu de production : $discircuit km<br>".
-            "<a href=\"index.php?action=panier&add=$id\"><button class=\"panier\" type='button'>Ajouter au panier</button></a></p>".
-            "</div><div class=\"majeur\">
+        $html .= "<style>.mineur {margin-left: 20px;}</style><h1 class=\"prod\">$name</h1><div class=\"mineur\"><p><br>Prix : $prix €<br>Provenance : $lieu<br>Description : <br><br>$desc<br>" .
+            "<br>Détails : $det<br>Distance du lieu de production : $discircuit km<br>" .
+            "<a href=\"index.php?action=panier&add=$id\"><button class=\"panier\" type='button'>Ajouter au panier</button></a></p>";
+        if (isset($_SESSION['user'])) if (unserialize($_SESSION['user'])->__get('role') == 1) $html .= "<a href='?action=editArticle&id=$id'><button class=\"panier\" type='button'>supprimer</button></a>";
+        $html .= "</div><div class=\"majeur\">
 <img class='image_pdt' src=\"src/img/$id.jpg\" alt=\"Image\">
 </div>";
 
---
+++ src/afficheurs/PageEditArticle.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace mywishlist\afficheurs;
+
+use mywishlist\db\ConnectionFactory;
+
+class PageEditArticle extends Afficheur
+{
+
+    public function execute(): string
+    {
+        if (!isset($_SESSION['user'])) return "<div class='titretexte'>droits insuffisants</div>>";
+        else if (unserialize($_SESSION['user'])->__get('role') == 0) return "<div class='titretexte'>droits insufifsants</div>>";
+        $html = "";
+        $db = ConnectionFactory::makeConnection();
+        $id = $_GET['id'];
+        $querry = "delete from produit where id = ?";
+        $req = $db->prepare($querry);
+        $req->execute([$id]);
+        $html .= "<div class='titretexte'>article supprimé</div>";
+        return $html;
+    }
+}

799d5ca1c0e985892fc7f6495f4b93bc4263feef
Egario-akier <thierry.dissidia@gmail.com>
2023-02-16 10:02:14 +0100

    dispatcher en mieux (bon renderPage)

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -71,27 +71,9 @@ class Dispatcher
         <html lang="fr">
         <head>
             <meta charset="UTF-8">
-            <title>NetVOD</title>
-            
-            <link rel="icon" type="image/png" sizes="64x64" href="src/Styles/img/favicon.ico">           
-            <link rel="stylesheet" href="src/Styles/CSS/tailwind.css"/>
-            <link rel="stylesheet" href="src/Styles/CSS/Catalogue.css"/>
+            <title>Court-Circuit Voltaire</title>
         </head>
         <body class="">
-        <header class=" text-gray-300 bg-gray-800 flex flex-row justify-between py-8 shadow-2xl mb-4 "  ">
-        <div class="  mx-8 ">
-            <a href="?action=accueil-utilisateur"><img class='' src="src/Styles/img/Petitlogo.png" width="10%"></a>
-        </div>
-        <div class="flex flex-row mx-8 ">
-            <div class="justify-start mx-8 "> 
-            <a href="?action=afficher-catalogue"><button class="rounded-2xl hover:bg-gray-300 hover:text-gray-800 m-2 p-1 px-3 h-full ">Catalogue</button></a>
-
-            </div>
-            <div  class=""> 
-               <a href="?action="><button class="rounded-2xl hover:bg-gray-300 hover:text-gray-800 m-2 p-1 px-3 h-full ">Se déconnecter</button></a>
-            </div>
-        </div>
-        </header>
         END;
 
         $res .= $html;

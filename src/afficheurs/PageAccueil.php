0d49d417cc8fcdc19d8e2a027d2ea37266ec8288
Benjamin Hugot <benjamin.hugot9@etu.univ-lorraine.fr>
2023-02-16 15:28:06 +0100

    No zoom here

--- src/afficheurs/PageAccueil.php
+++ src/afficheurs/PageAccueil.php
@@ -13,25 +13,25 @@ class PageAccueil extends Afficheur
 <img class= \"objet image\" src=\"src/img/8.jpg\" alt=\"Logo\">
 <div class=\"objet rectangle\"></div>
 <h1 class =\"objet titretexte\">Choisissez Bio</h1>
-<p class =\"objet soustexte\"> </p>
+<p class =\"objet soustexte\">Plus de 100 produits fait par vos commerçants préférés dans vos alentours prêt à vous fournir vos produits favoris</p>
 </div>
 <div class=\"pack1\">
-<img class= \"objet image1\" src=\"src/img/8.jpg\" alt=\"Logo\">
+<img class= \"objet image1\" src=\"src/img/3.jpg\" alt=\"Logo\">
 <div class=\"objet rectangle1\"></div>
-<h1 class =\"objet titretexte1\"> Pour aider vos éleveur favoris</h1>
-<p class =\"objet soustexte1\"> PTDR</p>
+<h1 class =\"objet titretexte1\">Vos produit favoris </h1>
+<p class =\"objet soustexte1\"> Des produits phares comme les pots de pâte à tartiner de la marque L'or et Nuts</p>
 </div>
 <div class=\"pack2\">
-<img class= \"objet image\" src=\"src/img/8.jpg\" alt=\"Logo\">
+<img class= \"objet image\" src=\"src/img/6.jpg\" alt=\"Logo\">
 <div class=\"objet rectangle\"></div>
-<h1 class =\"objet titretexte\"> Pour aider vos éleveur favoris</h1>
-<p class =\"objet soustexte\"> PTDR</p>
+<h1 class =\"objet titretexte\"> 100% bio et fait maison</h1>
+<p class =\"objet soustexte\"> Chaque grain de votre café est cultivé et récolté avec l'amour de vos producteurs et éleveurs proche de chez vous</p>
 </div>
 <div class=\"pack3\">
-<img class= \"objet image1\" src=\"src/img/8.jpg\" alt=\"Logo\">
+<img class= \"objet image1\" src=\"src/img/11.jpg\" alt=\"Logo\">
 <div class=\"objet rectangle1\"></div>
-<h1 class =\"objet titretexte1\"> Pour aider vos éleveur favoris</h1>
-<p class =\"objet soustexte1\"> PTDR</p>
+<h1 class =\"objet titretexte1\"> Et pas que la nourriture</h1>
+<p class =\"objet soustexte1\"> Mais des produits comme le savon, déodorant, bougie et bien plus encore vous attends ici</p>
 </div>
 ";
     }

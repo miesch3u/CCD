d25a67a909b53bb040c7fb00f79579506ba29627
Benjamin Hugot <benjamin.hugot9@etu.univ-lorraine.fr>
2023-02-16 15:18:14 +0100

    No zoom here

--- src/afficheurs/PageAccueil.php
+++ src/afficheurs/PageAccueil.php
@@ -12,8 +12,8 @@ class PageAccueil extends Afficheur
 <div class=\"pack\">
 <img class= \"objet image\" src=\"src/img/8.jpg\" alt=\"Logo\">
 <div class=\"objet rectangle\"></div>
-<h1 class =\"objet titretexte\"> Pour aider vos éleveur favoris</h1>
-<p class =\"objet soustexte\"> PTDR</p>
+<h1 class =\"objet titretexte\">Choisissez Bio</h1>
+<p class =\"objet soustexte\"> </p>
 </div>
 <div class=\"pack1\">
 <img class= \"objet image1\" src=\"src/img/8.jpg\" alt=\"Logo\">
